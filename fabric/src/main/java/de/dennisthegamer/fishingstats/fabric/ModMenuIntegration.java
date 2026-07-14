542f9ee0e4e1231a21e5ac18e7340f03bac9061b
dennisthegamer <41466710+dennisthegamer@users.noreply.github.com>
2026-07-14 20:43:02 +0200

    fix: guard config screen with isModLoaded (no broken ModMenuApi if YACL absent)

--- fabric/src/main/java/de/dennisthegamer/fishingstats/fabric/ModMenuIntegration.java
+++ fabric/src/main/java/de/dennisthegamer/fishingstats/fabric/ModMenuIntegration.java
@@ -3,11 +3,15 @@ package de.dennisthegamer.fishingstats.fabric;
 import de.dennisthegamer.fishingstats.config.FishingStatsConfigScreen;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        if (!FabricLoader.getInstance().isModLoaded("yet_another_config_lib_v3")) {
+            return parent -> null;
+        }
         return FishingStatsConfigScreen::create;
     }
 }
--- neoforge/src/main/java/de/dennisthegamer/fishingstats/neoforge/FishingStatsNeoForge.java
+++ neoforge/src/main/java/de/dennisthegamer/fishingstats/neoforge/FishingStatsNeoForge.java
@@ -8,6 +8,7 @@ import net.minecraft.resources.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
@@ -43,7 +44,9 @@ public final class FishingStatsNeoForge {
                 FishingStatsClient.onTick(Minecraft.getInstance()));
 
         // Config screen (equivalent of the ModMenu integration on Fabric; needs YACL installed)
-        container.registerExtensionPoint(IConfigScreenFactory.class,
-                (modContainer, parent) -> FishingStatsConfigScreen.create(parent));
+        if (ModList.get().isLoaded("yet_another_config_lib_v3")) {
+            container.registerExtensionPoint(IConfigScreenFactory.class,
+                            (modContainer, parent) -> FishingStatsConfigScreen.create(parent));
+        }
     }
 }
